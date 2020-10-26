e772afd563bf5b055bc3566208fd3bdb6f93be57
chenhao <hao_chen21@gmail.com>
2020-10-26 16:59:48 +0800

    add model

---
+++ src/main/java/cn/com/betasoft/dmp/batchprocess/domain/model/DataPoint.java
@@ -0,0 +1,31 @@
+package cn.com.betasoft.dmp.batchprocess.domain.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+@Builder
+@Table(value = "data_point")
+public class DataPoint {
+
+    @PrimaryKey
+    private DataPointKey dataPointKey;
+
+    @Column("tags")
+    private Map<String, String> tags = new HashMap<>();
+
+    @Column("value")
+    private Double value;
+
+    @Transient
+    private Integer ttl;
+}
---
+++ src/main/java/cn/com/betasoft/dmp/batchprocess/domain/model/DataPointKey.java
@@ -0,0 +1,30 @@
+package cn.com.betasoft.dmp.batchprocess.domain.model;
+
+import lombok.*;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@Builder
+@PrimaryKeyClass
+public class DataPointKey implements Serializable {
+
+    @PrimaryKeyColumn(name = "metric", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private String metric;
+
+    @PrimaryKeyColumn(name = "day", ordinal = 1, type = PrimaryKeyType.PARTITIONED)
+    private LocalDate day;
+
+    @PrimaryKeyColumn(name = "offset", ordinal = 2, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
+    private LocalTime offset;
+}
