e9bbc028df14a008a531a1ee0eba267e7d3f9a79
chenhao <hao_chen21@gmail.com>
2020-10-26 16:10:24 +0800

    Initial commit

---
+++ src/main/java/cn/com/betasoft/dmp/batchprocess/config/CassandraConfiguration.java
@@ -0,0 +1,69 @@
+package cn.com.betasoft.dmp.batchprocess.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
+import org.springframework.data.cassandra.core.cql.session.init.KeyspacePopulator;
+import org.springframework.data.cassandra.core.cql.session.init.ResourceKeyspacePopulator;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class CassandraConfiguration extends AbstractReactiveCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.port}")
+    private int port;
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keyspace;
+
+    @Value("${spring.data.cassandra.local-datacenter}")
+    private String dataCenter;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspace;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return dataCenter;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        return Arrays.asList(CreateKeyspaceSpecification
+                .createKeyspace(getKeyspaceName())
+                .ifNotExists(true)
+                .withSimpleReplication()
+                .with(KeyspaceOption.DURABLE_WRITES));
+
+    }
+
+    @Override
+    protected KeyspacePopulator keyspacePopulator() {
+        ResourceKeyspacePopulator populator = new ResourceKeyspacePopulator();
+        populator.setSeparator(";");
+        populator.setScripts(new ClassPathResource("cassandra/ddl-schema.cql"));
+        return populator;
+    }
+}
